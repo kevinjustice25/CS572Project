e116b646566eec774f641c18d65d5b286adc2c6c
Assefa <assefamit@gamil.com>
2020-03-17 05:25:47 -0500

    march 17 angular jwt work

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from './shared/auth.service';
 
 @Component({
   selector: 'app-root',
-  template: `
-            <p>{{title | uppercase}} </p>
-  `,
-  styleUrls: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular';
+
+  constructor(public authService: AuthService) { }
+
+  logout() {
+    this.authService.doLogout()
+  }
 }
